d6edce200851a234158334cb9f8034c6baeb86cd
Abel Tadesse <tadesseabel26@gmail.com>
2025-11-24 19:19:24 +0300

    feat: enhance counter store to include age state and persist data

--- Full-stack-second-batch/week-11_react-router-and-navigation/session-35_external-state-management/demo/src/state/counterStore.js
+++ Full-stack-second-batch/week-11_react-router-and-navigation/session-35_external-state-management/demo/src/state/counterStore.js
@@ -1,10 +1,20 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
-const initial = { count: 0 };
+const initial = { count: 0, age: 0 };
 
-export const useCounterStore = create((set) => ({
-  ...initial,
-  increment: () => set((s) => ({ count: s.count + 1 })),
-  decrement: () => set((s) => ({ count: s.count - 1 })),
-  reset: () => set(initial),
-}));
+export const useCounterStore = create(
+  persist(
+    (set) => ({
+      ...initial,
+      increment: () => set((s) => ({ count: s.count + 1 })),
+      decrement: () => set((s) => ({ count: s.count - 1 })),
+      reset: () => set(initial),
+    }),
+    {
+      name: "counter-storage",
+      partialize: (state) => ({ ...state }), // only persist count
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);

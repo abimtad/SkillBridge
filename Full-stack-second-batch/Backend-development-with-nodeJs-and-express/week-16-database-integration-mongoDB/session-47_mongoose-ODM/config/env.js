bf238b808984df4957be99afa26a30b6f1e509ba
Abel Tadesse <tadesseabel26@gmail.com>
2026-01-10 19:59:24 +0300

    session 47 student version

--- Full-stack-second-batch/Backend-development-with-nodeJs-and-express/week-16-database-integration-mongoDB/session-47_mongoose-ODM/config/env.js
+++ Full-stack-second-batch/Backend-development-with-nodeJs-and-express/week-16-database-integration-mongoDB/session-47_mongoose-ODM/config/env.js
@@ -2,11 +2,9 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Needed to make __dirname work in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Load .env file manually
 dotenv.config({
   path: path.join(__dirname, "../../.env"),
 });
--- Full-stack-second-batch/Backend-development-with-nodeJs-and-express/week-16-database-integration-mongoDB/session-47_mongoose-ODM/controllers/student.controller.js
+++ Full-stack-second-batch/Backend-development-with-nodeJs-and-express/week-16-database-integration-mongoDB/session-47_mongoose-ODM/controllers/student.controller.js
@@ -3,7 +3,6 @@ const Student = require("../models/student.model");
 // CREATE
 exports.createStudent = async (req, res) => {
   try {
-    const student = await Student.create(req.body);
     res.status(201).json({ success: true, student });
   } catch (err) {
     res.status(400).json({ success: false, error: err.message });
@@ -12,24 +11,17 @@ exports.createStudent = async (req, res) => {
 
 // READ ALL
 exports.getStudents = async (req, res) => {
-  const students = await Student.find();
   res.json(students);
 };
 
 // READ ONE
 exports.getStudent = async (req, res) => {
-  const student = await Student.findById(req.params.id);
   if (!student) return res.status(404).json({ message: "Not found" });
   res.json(student);
 };
 
 // UPDATE
 exports.updateStudent = async (req, res) => {
-  const student = await Student.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true, runValidators: true }
-  );
 
   if (!student) return res.status(404).json({ message: "Not found" });
 
@@ -38,7 +30,6 @@ exports.updateStudent = async (req, res) => {
 
 // DELETE
 exports.deleteStudent = async (req, res) => {
-  const student = await Student.findByIdAndDelete(req.params.id);
 
   if (!student) return res.status(404).json({ message: "Not found" });
 
